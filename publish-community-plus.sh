1faa11da48da992cb6a2e5feb21ccb5691dbd192
Alexander Merkulov <sasha@merqlove.ru>
2019-11-23 12:12:11 +0300

    Add deploy method

---
+++ publish-community-plus.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+cd enterprise-modules/grid-set-filter
+npm install
+npm run build
+npm run package
+npm publish --registry=$NPM_REGISTRY
+
+exit 0
---
+++ unpublish-community-plus.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd enterprise-modules/grid-set-filter
+npm unpublish --registry=$NPM_REGISTRY --force
+
+exit 0
